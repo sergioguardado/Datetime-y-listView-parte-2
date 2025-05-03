30cea60b72ae43b6e22cda1a6e4ccf53594d2d09
sergioguardado <sergio.guardado.002@gmail.com>
2025-05-02 20:53:22 -0600

    Add files via upload

---
+++ Listview/ListView/ListView/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace ListView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstvAlimento = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // lstvAlimento
+            // 
+            this.lstvAlimento.HideSelection = false;
+            this.lstvAlimento.Location = new System.Drawing.Point(35, 12);
+            this.lstvAlimento.Name = "lstvAlimento";
+            this.lstvAlimento.Size = new System.Drawing.Size(440, 308);
+            this.lstvAlimento.TabIndex = 0;
+            this.lstvAlimento.UseCompatibleStateImageBehavior = false;
+            this.lstvAlimento.SelectedIndexChanged += new System.EventHandler(this.lstvAlimento_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1142, 494);
+            this.Controls.Add(this.lstvAlimento);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lstvAlimento;
+    }
+}
+
---
+++ Listview/ListView/ListView/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListView
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lstvAlimento_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ListViewGroup frutas = new ListViewGroup("frutas", HorizontalAlignment.Left); 
+            ListViewGroup carnes =  new ListViewGroup("carnes", HorizontalAlignment.Left);
+
+            lstvAlimento.Items.Add(new ListViewItem("Manzana", frutas));
+            lstvAlimento.Items.Add(new ListViewItem("Pera", frutas));
+            lstvAlimento.Items.Add(new ListViewItem("Sandia", frutas));
+            lstvAlimento.Items.Add(new ListViewItem("Banana", frutas));
+            lstvAlimento.Items.Add(new ListViewItem("Melon", frutas));
+            lstvAlimento.Items.Add(new ListViewItem("ciruela", frutas));
+
+            ListViewItem miElemento = new ListViewItem("pollo", carnes);
+            lstvAlimento.Items.Add(miElemento);
+
+            lstvAlimento.Items.Add(new ListViewItem("Res", carnes));
+            lstvAlimento.Items.Add(new ListViewItem("Pescado", carnes));
+            lstvAlimento.Items.Add(new ListViewItem("Cerdo", carnes));
+            lstvAlimento.Items.Add(new ListViewItem("Codorniz", carnes));
+
+            lstvAlimento.Groups.Add(frutas);
+            lstvAlimento.Groups.Add(carnes);
+        }
+    }
+}
